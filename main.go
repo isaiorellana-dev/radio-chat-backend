fa77f831bef68f893dbb8932415b0a0a2e9dacf6
isai <isaiorellana.dev@gmail.com>
2023-07-16 14:29:10 -0600

    🚚 Stop loading .env

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/isaiorellana-dev/radio-chat-backend/routes"
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -18,10 +16,10 @@ func main() {
 
 	routes.RegisterRoutes(e)
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 	PORT := os.Getenv("PORT")
 
 	if err := e.Start(PORT); err != nil {
