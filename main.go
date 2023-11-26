db18b3540af07fd5079ed98f2ce85773e6c9db0f
isaiorellana-dev <isaiorellana.dev@gmail.com>
2023-11-26 12:52:19 -0600

    CORS whitelist

--- main.go
+++ main.go
@@ -13,8 +13,22 @@ import (
 func main() {
 	e := echo.New()
 
+	if os.Getenv("ENVIRONMENT") == "development" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	PORT := os.Getenv("PORT")
+	FRONTEND_APP := os.Getenv("FRONTEND_APP")
+
 	e.Use(middleware.Logger())
-	e.Use(middleware.CORS())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{FRONTEND_APP},
+		AllowHeaders: middleware.DefaultCORSConfig.AllowHeaders,
+		AllowMethods: middleware.DefaultCORSConfig.AllowMethods,
+	}))
 
 	routes.RegisterRoutes(e)
 
@@ -24,8 +38,6 @@ func main() {
 			log.Fatal("Error loading .env file")
 		}
 	}
-
-	PORT := os.Getenv("PORT")
 
 	if err := e.Start(":" + PORT); err != nil {
 		e.Logger.Fatal(err)
